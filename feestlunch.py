e3d3262bcb2f81676cdc0379975c665acbb495fd
Piotrek <99058308@mydavinci.nl>
2021-09-27 09:15:53 +0200

    Git

---
+++ feestlunch.py
@@ -0,0 +1,8 @@
+Croissantje = 0.39
+Aantalcroissantjes = 17
+Stokbrood = 2.78
+Aantalstokbrooden = 2
+Kortingsbon = 0.50
+Aantalbonnen = 3
+
+print (Aantalcroissantjes*Croissantje+Aantalstokbrooden*Stokbrood+Aantalbonnen*Kortingsbon)
---
+++ speelhaldag.py
@@ -0,0 +1,7 @@
+Toegangsticket = 7.45
+Personen = 4
+Pervijfminuten = 0.37
+Speeltijd = 45
+Aantalmin = 5
+
+print (Toegangsticket*Personen+Speeltijd/Aantalmin*Pervijfminuten*Personen)
